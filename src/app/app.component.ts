ef6b638674341fe171acbef95824fcce3bd53449
pbecchetti <pbecchetti@adscientiam.com>
2022-04-01 12:07:32 +0200

    Added my project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'paulinebecchetti';
+  title = 'pauline becchetti website';
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let element = document.querySelector('.header') as HTMLElement;
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > 100) {
+      element.classList.add('scrolled');
+    } else {
+      element.classList.remove('scrolled');
+    }
+  }
 }
